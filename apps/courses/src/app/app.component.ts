4503c2237388e53699cc27cdb58c74f61365a39f
Lilian Dyer <ldyer@healthequity.com>
2020-07-29 09:31:00 -0600

    initial commit

---
+++ apps/courses/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sun-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'courses';
+}
